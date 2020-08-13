5ef741d91a8bac764f5d0670410c148232eb5ddd
Jean-Francois Rose <jf.rose@nutaq.com>
2020-08-13 16:21:45 -0400

    JFR - Fixed a bug which caused the previous command to latch only after the current one is executed

--- src/spintop_openhtf/plugs/telnet.py
+++ src/spintop_openhtf/plugs/telnet.py
@@ -25,19 +25,16 @@ def _telnet_client_connected(function):
 
 class TelnetInterface(UnboundPlug):
 
-    def __init__(self, addr, username, password, port):
+    def __init__(self, addr, port=4343):
         super().__init__()
         self.tn = None
         self.addr = addr
         self.port = port
-        self.username = username
-        self.password = password
 
     def open(self, _client=None):
         # _client allows to pass in a mock for testing
         self.logger.info("(Initiating Telnet connection at %s)", self.addr)
-        self.logger.info("(addr={}:{}, user={!r}, password={!r})".format(
-            self.addr, self.port, self.username, self.password))
+        self.logger.info("(addr={}:{})".format(self.addr, self.port))
         try:
             if _client is None:
                 _client = telnetlib.Telnet(self.addr, self.port)
@@ -55,7 +52,7 @@ class TelnetInterface(UnboundPlug):
 
     def is_connected(self):
         try:
-            self.tn.write("\r\n")
+            self.tn.write("\r\n".encode())
             # If the Telnet object is not connected, an AttributeError is raised
         except AttributeError:
             return False
@@ -63,17 +60,17 @@ class TelnetInterface(UnboundPlug):
             return True
 
     @_telnet_client_connected
-    def execute_command(self, command: str, timeout: float = 10, targets=None):
+    def execute_command(self, command: str, expected_output: str = "", until_timeout: float = 5):
         """Send a :obj:`command` and wait for it to execute.
 
         Args:
             command (str): The command to send. End of lines are automatically managed. For example execute_command('ls')
             will executed the ls command.
-            timeout (float, optional): The timeout in second to wait for the command to finish executing. Defaults to 10.
-            targets ([type], optional): [description]. Defaults to None.
+            expected_output (str, optional): If not none, the command executor will read until a given string, expected_output, is encountered or until until_timeout (s) have passed.
+            until_timeout (float, optional): The timeout in seconds to wait for the command to finish reading output. Defaults to 5.
 
         Returns:
-            str: output response from the Telnet client
+            output (str): output response from the Telnet client
 
         Raises:
             TelnetError:
@@ -82,17 +79,20 @@ class TelnetInterface(UnboundPlug):
 
         output = ""
         if command != "":
-            self.logger.info("(Timeout %.1fs)" % (timeout))
-            self.tn.write((command + "\n").encode('utf-8'))
+            self.logger.info("(Timeout %.1fs)" % (until_timeout))
+            self.tn.write((command).encode() + b"\r\n")
             self.logger.debug("> {!r}".format(command))
 
             try:
-                output = self.tn.read_very_eager()
-                output = output.decode('utf-8')
+                if expected_output == "":
+                    output = self.tn.read_until(b'\n\n', until_timeout)
+                    output = output.decode()
+                else:
+                    expected_output = expected_output.encode()
+                    output = self.tn.read_until(expected_output, until_timeout)
+                    output = output.decode()
             except Exception as e:
                 raise TelnetError(e)
-            finally:
-                self.tn.close()
         else:
             pass
 
