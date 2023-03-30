c873aaca72844b32a7dadfde0b8741254d5b134c
Marco van Noord <m.vannoord@pr-electronics.nl>
2023-03-30 09:35:43 +0200

    started merge

--- src/openhtf/output/web_gui/src/app/stations/station/test-summary.component.ts
+++ src/openhtf/output/web_gui/src/app/stations/station/test-summary.component.ts
@@ -1,4 +1,20 @@
 /**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * Widget displaying summary information about a test.
  */
 
@@ -33,9 +49,11 @@ export class TestSummaryComponent implements OnChanges {
 
     let completedPhases = 0;
     for (const phase of this.test.phases) {
-      if (phase.status > PhaseStatus.running) {
-        completedPhases++;
+      if (phase.status === PhaseStatus.running ||
+          phase.status === PhaseStatus.fail) {
+        break;
       }
+      completedPhases++;
     }
     return completedPhases;
   }
