0b1c5003395ce51bfb1e7940d0f950ddf3c1ced6
Marco van Noord <m.vannoord@pr-electronics.nl>
2023-03-30 08:38:57 +0200

    add Tornado to setup-requirements fixed wrong assert made layout slightly wider

--- setup.py
+++ setup.py
@@ -77,7 +77,9 @@ setup(
         'protobuf>=3.6.0,<4.0',
         'PyYAML>=5.0',
         'pytz',
-        'pyOpenSSL>=17.1.0,<18.0'
+        'pyOpenSSL>=17.1.0,<18.0',
+        'tornado>=6.0',
+        'sockjs-tornado>=1.0.3,<2.0'
     ],
     extras_require={
         'server':[ # Allows the different GUI servers. Can still run without GUI.
